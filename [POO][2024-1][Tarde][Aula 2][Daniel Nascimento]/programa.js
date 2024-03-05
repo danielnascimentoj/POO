d50fcee2c1b0263d2c54beda5b7f9bec27756b23
danielnascimentoj <159052431+danielnascimentoj@users.noreply.github.com>
2024-03-05 16:28:13 -0300

    ALLT

---
+++ [POO][2024-1][Tarde][Aula 2][Daniel Nascimento]/programa.js
@@ -0,0 +1,113 @@
+/*let alunos = ["João","Maria","Jose","Pedro","Paulo"]
+
+//alert(alunos[3])
+
+alunos[4] = "Gerson"
+console.log(alunos)
+alunos.push("Joaquim")
+
+let notas = [10,8,5,7,9,6]
+console.log(notas)
+
+for(let i = 0;i<=alunos.length;i++){
+  console.log("Alunos: " + alunos[i] + "Notas: " + notas[i])
+}
+*/
+
+
+//_____________________________________________________________ F I M _______________________________________________________________________//
+let aluno1 = {
+    nome: "Mateus", 
+    cpf: "126.342.423-76", 
+    nota:10
+}
+
+let aluno2 = {
+    nome: "Marcos", 
+    cpf: "128.417.784-87", 
+    nota:12
+}
+
+let aluno3 = {
+    nome: "Lucas", 
+    cpf: "102.014.432-21", 
+    nota:20
+}
+
+let aluno4 = {
+    nome: "João", 
+    cpf: "126.572.986-98", 
+    nota:10
+}
+
+let aluno5 = {
+    nome: "Daniel", 
+    cpf: "126.573.983-91", 
+    nota:1000
+}
+
+let aluno6 = {
+    nome: "Pedro", 
+    cpf: "126.419.653-76", 
+    nota:1
+}
+
+                                                            //Acessando atributos de um objeto
+                                                            //Em um array, a gente acessa um elemento
+                                                            //Através de um colcheto [] e do indice.
+
+console.log(aluno1.nome)
+console.log(aluno1.cpf)
+console.log(aluno1.nota)
+//_____________________________________________________________ F I M _______________________________________________________________________//
+                                                        
+                                                        //ATIVIDADE DE CARROS
+
+let carro1 = {
+    modelo: "Hilux",
+    marca: "TOYOTA",
+    ano: "2023",
+    quilometragem:"7.000Km",
+    cor: "Preto",
+    preco: "389.000.000"
+}
+
+let carro2 = {
+    modelo: "TIGGO 8 PRO",
+    marca: "CAOA CHERY",
+    ano: "2023",
+    quilometragem:"0Km",
+    cor: "Branco",
+    preco: "326.700.000"
+}
+
+let carro3 = {
+    modelo: "Q5",
+    marca: "Audi",
+    ano: "2O19/2020",
+    quilometragem:"30.000Km",
+    cor: "Branco",
+    preco: "215.000.000"
+}
+let carro4 = {
+    modelo: "X5",
+    marca: "BMW",
+    ano: "2017",
+    quilometragem:"95.000Km",
+    cor: "Cinza",
+    preco: "210.000.000"
+}
+let carro5 = {
+    modelo: "911",
+    marca: "Porshe",
+    ano: "2023",
+    quilometragem:"0Km",
+    cor: "Cinza",
+    preco: "1.179.000"
+}
+
+//__________________________________________________________________________________
+
+
+
+
